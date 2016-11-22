a9d520b62edf12de7791f3eab371d36bedc1400d
kabrial <kevin.abrial@etud.univ-st-etienne.fr>
2016-11-22 14:10:56 +0100

    Add MY_BANK_IDENTIFIER

--- BankTransactionProject/src/main/java/com/ujm/xmltech/utils/BankSimulationConstants.java
+++ BankTransactionProject/src/main/java/com/ujm/xmltech/utils/BankSimulationConstants.java
@@ -5,24 +5,24 @@ public class BankSimulationConstants {
   /**
    * Directory where are files to process
    */
-  public final static String IN_DIRECTORY = System.getProperty("user.dir")+"/../../folders/in/";
+  public final static String IN_DIRECTORY = System.getProperty("user.dir")+"/../../BankTransactionProject/folders/in/";
   /**
    * Directory where are reports
    */
-  public final static String OUT_DIRECTORY = System.getProperty("user.dir")+"/../../folders/out/";
+  public final static String OUT_DIRECTORY = System.getProperty("user.dir")+"/../../BankTransactionProject/folders/out/";
   /**
    * Directory where are files under process
    */
-  public final static String WORK_DIRECTORY = System.getProperty("user.dir")+"/../../folders/work/";
+  public final static String WORK_DIRECTORY = System.getProperty("user.dir")+"/../../BankTransactionProject/folders/work/";
   /**
    * Directory where are files already processed
    */
-  public final static String ARCHIVE_DIRECTORY = System.getProperty("user.dir")+"/../../folders/archive/";
+  public final static String ARCHIVE_DIRECTORY = System.getProperty("user.dir")+"/../../BankTransactionProject/folders/archive/";
 
   /**
    * must contain only 4 upper case letters. Real example : BNPP
    */
-  public final String MY_BANK_IDENTIFIER = "NYCU";
+  public final String MY_BANK_IDENTIFIER = "JAKY";
 
   /**
    * persistence unit name in the spring configuration
